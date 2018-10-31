0427a2b3686c1d3dd26fedd4224d8839a7489532
gldnbxy <daggem.a.samuel@gmail.com>
2018-10-31 19:12:01 +0000

    Can drop down countries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CountriesComponent } from './countries/countries.component';
@@ -12,7 +13,8 @@ import { StatesComponent } from './states/states.component';
     StatesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
