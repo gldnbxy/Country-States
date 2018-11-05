db49d72e8c49eae09a1d940b140ede79004ae618
gldnbxy <daggem.a.samuel@gmail.com>
2018-11-05 20:23:23 +0000

    literally thought I was going to die but no

--- src/app/countries/countries.component.ts
+++ src/app/countries/countries.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../Country';
+import { CountryService } from '../country.service';
 
 @Component({
   selector: 'app-countries',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CountriesComponent implements OnInit {
 
-  constructor() { }
+  countries: Country[];
+
+  constructor( private countryService: CountryService ) { }
+
+  getCountries(): void {
+    this.countryService.getCountries().subscribe(countries => this.countries = countries);
+  }
 
   ngOnInit() {
+    this.getCountries();
   }
 
 }
