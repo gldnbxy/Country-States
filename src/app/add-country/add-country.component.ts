bee8aa55e8473045edcb91fcc597d723f08b297e
gldnbxy <daggem.a.samuel@gmail.com>
2018-11-14 20:36:17 +0000

    wip

--- src/app/add-country/add-country.component.ts
+++ src/app/add-country/add-country.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../country';
+import { CountryService } from '../country.service';
 
 @Component({
   selector: 'app-add-country',
@@ -7,7 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCountryComponent implements OnInit {
 
-  constructor() { }
+  add(countryId: number, countryCode: string, countryName: string)
+  {
+    console.log(`ID: ${countryId}, Code: ${countryCode}, Name: ${countryName}`);
+  }
+
+  constructor(private acountry: Country, countryService: CountryService) { }
 
   ngOnInit() {
   }
--- src/app/country.service.ts
+++ src/app/country.service.ts
@@ -14,11 +14,21 @@ const httpOptions = {
 
 export class CountryService {
 
+  //a list of countries
+  private countries: Country[];
+
   //URL from whence countries shall be fetched
   private cUrl = 'https://xc-ajax-demo.herokuapp.com/api/countries/'
 
   getCountries(): Observable<Country[]> {
     return this.http.get<Country[]>(this.cUrl);
   }
+
+  addCountry(cid: number, cCode: string, cName: string): boolean
+  {
+    this.http.get<Country[]>(this.cUrl).subscribe(countries => this.countries = countries);
+    return false;
+  }
+
   constructor(private http: HttpClient) { }
 }
